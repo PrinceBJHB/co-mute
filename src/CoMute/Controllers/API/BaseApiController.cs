f9962bb7273cdfa5a1237a94405fe57629c7d269
DrMetu <metu@live.co.za>
2015-09-10 17:08:22 +0200

    Files that never got pushed with the first commit for some reason

---
+++ src/CoMute/Controllers/API/BaseApiController.cs
@@ -0,0 +1,72 @@
+﻿using CoMute.Web.App_Start;
+using CoMute.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity;
+
+namespace CoMute.Web.Controllers.API
+{
+    public class BaseApiController : ApiController
+    {
+        private ModelFactory _modelFactory;
+        private UserManager _AppUserManager = null;
+
+        protected UserManager AppUserManager
+        {
+            get
+            {
+                return _AppUserManager ?? Request.GetOwinContext().GetUserManager<UserManager>();
+            }
+        }
+
+        public BaseApiController()
+        {
+        }
+
+        protected ModelFactory TheModelFactory
+        {
+            get
+            {
+                if (_modelFactory == null)
+                {
+                    _modelFactory = new ModelFactory(this.Request, this.AppUserManager);
+                }
+                return _modelFactory;
+            }
+        }
+
+        protected IHttpActionResult GetErrorResult(IdentityResult result)
+        {
+            if (result == null)
+            {
+                return InternalServerError();
+            }
+
+            if (!result.Succeeded)
+            {
+                if (result.Errors != null)
+                {
+                    foreach (string error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+
+                if (ModelState.IsValid)
+                {
+                    // No ModelState errors are available to send, so just return an empty BadRequest.
+                    return BadRequest();
+                }
+
+                return BadRequest(ModelState);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/CoMute/Controllers/API/CarPoolController.cs
@@ -0,0 +1,106 @@
+﻿using CoMute.Web.Models;
+using CoMute.Web.Models.Dto;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Linq;
+using CoMute.Web.Models.EntityFramework;
+using System.Collections;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace CoMute.Web.Controllers.API
+{
+    public class CarPoolController : ApiController
+    {
+        [AllowAnonymous]
+        [Route("api/carpool/create")]
+        public HttpResponseMessage CreateCarPool(CarPoolViewModel model)
+        {
+            using (CoMuteEntities db = new CoMuteEntities())
+            {
+                try
+                {
+                    if (model.CarPools.ID == 0)
+                    {
+
+                        //Add new
+                        CarPools carpools = new CarPools();
+                        CarPoolDays carpooldays = new CarPoolDays();
+
+                        carpools.AvailableSeats = model.CarPools.AvailableSeats;
+                        carpools.CarPoolDays = model.CarPools.CarPoolDays;
+                        carpools.DateCreated = model.CarPools.DateCreated;
+                        carpools.DepartureTime = model.CarPools.DepartureTime;
+                        carpools.Destination = model.CarPools.Destination;
+                        carpools.ETA = model.CarPools.ETA;
+                        carpools.Notes = model.CarPools.Notes;
+                        carpools.Origin = model.CarPools.Origin;
+                        carpools.OwnerID = model.CarPools.OwnerID;
+
+                        carpooldays.DayOfWeek = model.CarPoolDays.DayOfWeek;
+                        carpooldays.CarPoolID = model.CarPools.ID;
+
+                        db.CarPools.Add(carpools);
+                        db.CarPoolDays.Add(carpooldays);
+
+                        db.SaveChanges();
+
+                    }
+                    else
+                    {
+                        //Update
+                        var carpools = (from n in db.CarPools
+                                             where n.ID == model.CarPools.ID
+                                             select n).SingleOrDefault();
+
+                        var carpooldays = (from n in db.CarPoolDays
+                                           where n.CarPoolID == model.CarPools.ID
+                                        select n).SingleOrDefault();
+
+                        carpools.AvailableSeats = model.CarPools.AvailableSeats;
+                        carpools.CarPoolDays = model.CarPools.CarPoolDays;
+                        carpools.DateCreated = model.CarPools.DateCreated;
+                        carpools.DepartureTime = model.CarPools.DepartureTime;
+                        carpools.Destination = model.CarPools.Destination;
+                        carpools.ETA = model.CarPools.ETA;
+                        carpools.Notes = model.CarPools.Notes;
+                        carpools.Origin = model.CarPools.Origin;
+                        carpools.OwnerID = model.CarPools.OwnerID;
+
+                        carpooldays.DayOfWeek = model.CarPoolDays.DayOfWeek;
+
+                        db.CarPools.Add(carpools);
+                        db.CarPoolDays.Add(carpooldays);
+                        db.SaveChanges();
+                    }
+                    return Request.CreateResponse(HttpStatusCode.Accepted);
+                }
+                catch (Exception ex)
+                {
+                    return Request.CreateResponse(ex);
+                }
+            };
+        }
+
+        [Route("api/carpool/get")]
+        [HttpPost]
+        public IEnumerable getCarPools()
+        {
+
+            using (CoMuteEntities db = new CoMuteEntities())
+            {
+                var n = from t in db.CarPools
+                        select t;
+
+                return n.ToList();
+            }
+
+        }
+
+    }
+}
