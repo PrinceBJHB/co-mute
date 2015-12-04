4b16eb7510dfa87768b5b08702ba297919fb41eb
Richard Trevor <rktrevor@gmail.com>
2015-12-04 10:16:17 +0200

    1) Added model for Location 2) Added model for Car Pool Creation

---
+++ src/CoMute.Models/CarPool.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoMute.Models
+{
+    public class CarPool
+    {
+        public Guid CarPoolID{ get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime ExpectedArrivalTime { get; set; }
+        public virtual Location Origin { get; set; }
+        public int DaysAvailable { get; set; }
+        public virtual Location Destination { get; set; } 
+        public int AvailableSeats { get; set; }
+        public virtual User Owner { get; set; }
+        public string Notes { get; set; }
+    }
+}
